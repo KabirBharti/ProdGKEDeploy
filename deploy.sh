e4b641e9e01c31af895a62260723c8d38680b371
Kabir Bharti <bharti.kabir@gmail.com>
2020-11-22 14:55:45 -0600

    Added Deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 docker build -t evilfiend/multi-client:latest -t evilfiend/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t evilfiend/multi-server:latest -t evilfiend/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t evilfiend/multi-worker:latest -t evilfiend/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-
 docker push evilfiend/multi-client:latest
 docker push evilfiend/multi-server:latest
 docker push evilfiend/multi-worker:latest
